2468bb56d27882a6eb4e1a5e0b106d3c136284f0
Abdullah1353 <rehmanabdullah355@gmail.com>
2021-10-27 13:10:06 +0500

    initial commit

---
+++ sg_skip_installment_portal/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': "SG Skip Installment Portal",
+    'summary': """ SG Skip Installment Portal """,
+    'description': """
+
+    """,
+    'category': '',
+    'version': '1.0',
+    'depends': ['hr', 'base', 'sync_employee_advance_salary', 'portal', 'website'],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/data.xml',
+        'views/assets.xml',
+        # 'views/advance_approval_view.xml',
+        'views/skip_request_form.xml',
+
+    ],
+    'demo': [
+
+    ],
+    'qweb': [
+
+    ],
+    'js': ['static/src/js/advance_portal.js'],
+
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
